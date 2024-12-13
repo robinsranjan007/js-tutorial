521281630e01e63eb5c04e04b3859763e89420f1
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-13 01:22:03 -0500

    lec-07-added

---
+++ lec-7.js
@@ -0,0 +1,16 @@
+const name ="robins"
+
+const repocount =45;
+
+console.log(`my name is ${name} and my repo count is ${repocount}`)
+
+const gameName = new String('RobinsRR')
+
+console.log(gameName)
+console.log(gameName[0],name[0])
+console.log(name.__proto__)
+
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.charAt(1))
+console.log(name.indexOf('r'))
