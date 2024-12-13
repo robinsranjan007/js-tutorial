2620e8d7745917c2ec6f66a09a7529497e712cd5
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-12 22:03:34 -0500

    Added-lec 04

---
+++ lec-4.js
@@ -0,0 +1,22 @@
+// **********************OPERATIONS*******************************
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+
+
+let str1= "hello "
+let str2= "Robins"
+
+let str3 = str1 +str2;
+
+console.log(str3)
+
+console.log(1+"2")
+console.log(2+"1")
+console.log(2+2+"2")
+console.log("1"+2+2)
+console.log("2"+"2"+2)
+console.log(typeof(2+"1"),typeof("1"+2))
+console.log(+true)
+console.log(+"")
