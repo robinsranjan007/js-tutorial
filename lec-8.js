b178fbd60948e55aba74c1632e212630df277dae
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-13 01:49:06 -0500

    lec-08-added

---
+++ lec-8.js
@@ -0,0 +1,20 @@
+const score = 400
+
+const balance = new Number(500)
+
+console.log(balance)
+
+console.log('1234'.toLocaleUpperCase())
+
+let otherNumber = 1234.5454
+
+console.log(otherNumber.toFixed(3))
+
+
+let newNumber = 123.567
+
+console.log( newNumber.toPrecision())
+
+const somenumber = 2343
+
+console.log(somenumber.toLocaleString('en-IN'))
