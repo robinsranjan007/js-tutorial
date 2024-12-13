e5c1d76574de7583f63f614b4cb4de325841e48e
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-12 20:19:54 -0500

    Added-lec 01

---
+++ lec-1.js
@@ -0,0 +1,19 @@
+
+//  LET CONST VAR
+
+const accountid=12334 //1
+let accountEmail ="robinsranjan@gmail.com" //2
+var accountPassword= 1234 //3
+name ="robisn" //4
+
+let fname ; // this is undefined
+
+console.log(name)
+
+
+ 
+
+let accountEmails="ranjan"
+
+
+// prefer not to use var cause of block and global scope
