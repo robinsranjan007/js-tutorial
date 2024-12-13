5b02190fd3d7745b129bb35eb7c02921125505bd
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-12 20:36:10 -0500

    Added-lec 02

---
+++ lec-2.js
@@ -0,0 +1,40 @@
+// DATA TYPES
+
+"use strict" //treat all js code as new version
+
+// alert("hello") 
+ // it will give error as this is not supported in the node js it is supporte in the browser
+
+
+// DATA TYPES
+
+var name= "robins"  //string
+let age = 56; //number
+let isloggedIn = true //boolean
+
+// null this is a standalone value , or empty
+
+let temp = null //this is empty value
+
+
+let state //undefined
+
+//symbol
+
+//PRIMITIVE DATATYPES
+//string
+//boolean
+//number
+//bigint
+//null
+//undefined
+//sybol
+
+
+//object
+
+
+
+console.log(typeof "helloe")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
