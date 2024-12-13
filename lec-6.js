716810da02c315392ed1dfea82ee3165277c63d1
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-13 01:02:46 -0500

    lec-06-added

---
+++ lec-6.js
@@ -0,0 +1,11 @@
+// DATA TYPES SUMMARY  js is dynamically typed the data type is determined at runtime
+//  type script is statically typed you need to write the data type at the time of declaring the variable
+
+// PRIMITIVE DATA TYPES (changes in the copy values not the original values)
+// 7:TYPES string, number,boolean,null(khali),undefined,Symbol,BigInt
+
+// Reference data types of Non-primitive data types
+//arrays,Objects,Functions,
+
+
+ 
